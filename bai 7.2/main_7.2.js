f9ac9f4ba9ff53e8c801bdcdca8afd7149e89fcb
hungfdfs <hiepga11042003@gmail.com>
2025-05-09 10:37:02 +0700

    bài 7.2

---
+++ bai 7.2/main_7.2.js
@@ -0,0 +1,16 @@
+//7.2 các phép tính cơ bản
+let a = 5;
+let b = 2;
+
+let tong = a + b; // tong = 7
+let hieu = a - b; // hieu = 3
+let tich = a * b; // tich = 10
+let thuong = a / b; // thuong = 2.5
+let soDu = a % b; // dư = 1
+
+// xuất kết quả
+console.log("tổng 2 số: " + tong);
+console.log("hiệu 2 số: " + hieu);
+console.log("tích 2 số: " + tich);
+console.log("thương 2 số: " + thuong);
+console.log("dư của phép chia a cho b: " + soDu);
