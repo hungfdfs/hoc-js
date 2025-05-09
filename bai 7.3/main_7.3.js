90abb20c34571d1a46910cf8030c0859b9ab038b
hungfdfs <hiepga11042003@gmail.com>
2025-05-09 15:43:10 +0700

    bai 7.3

---
+++ bai 7.3/main_7.3.js
@@ -0,0 +1,10 @@
+// Ưu tiên ()
+let diemToan = 9.5;
+let diemVan = 8.75;
+let dtb = (diemToan + diemVan) / 2;
+console.log(dtb);
+
+// right to left
+let x = (y = 25);
+// left to right
+let c = 257 + 8 - 1;
