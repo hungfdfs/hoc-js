9f7c6c1eb2268baebb6e30969ea900fc673c2349
hungfdfs <hiepga11042003@gmail.com>
2025-05-09 09:50:05 +0700

    bai 6

---
+++ bai 6/main_6.js
@@ -0,0 +1,32 @@
+// kieu string
+let userName = "Nguyen Van A";
+
+//kieu number
+let soNguyen = 10;
+let soThuc = 10.5;
+
+//kieu boolean
+let check = true;
+
+// kieu undefined
+let diemToan;
+
+// kieu null
+let connect = null;
+
+//xuat ra man hinh
+console.log(userName);
+console.log(typeof userName);
+console.log(soNguyen);
+console.log(soThuc);
+console.log(typeof soNguyen);
+console.log(typeof soThuc);
+
+console.log(check);
+console.log(typeof check);
+console.log(typeof diemToan);
+console.log(typeof connect);
+
+// xuat ra so nguyen an toan toi da/toi thieu
+console.log("so nguyen an toan toi da: " + Number.MAX_SAFE_INTEGER);
+console.log("so nguyen an toan toi thieu: ", Number.MIN_SAFE_INTEGER);
